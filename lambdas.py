74beea2a3f90755fadef8f35ec18513fb63ca50c
Paulina Grunwald <paulina.grunwald@quasset.com>
2019-02-23 07:54:18 +0100

    Add example using lambda function

---
+++ lambdas.py
@@ -0,0 +1,8 @@
+
+x = 10
+y = 5
+z = 2
+divide = lambda x, y: int(x / y)
+print('Result of division {}/{} is {}'.format(x,y, divide(x, y)))
+add = lambda x,y,z : x+y+z
+print('Result of adding {},{},{} is {}'.format(x,y,z, add(x, y,z)))
